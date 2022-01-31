c641846b2fefa68ece225ad6a88551ef42045ab8
imjonrourke <jonpng11@gmail.com>
2022-01-30 21:33:31 -0500

    feat(ex-1): rendering index and starting loop from os.Args[0]

--- ch1/echo1/main.go
+++ ch1/echo1/main.go
@@ -10,12 +10,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i] + " " + strconv.Itoa(i)
 		sep = " "
 	}
 	fmt.Println(s)
