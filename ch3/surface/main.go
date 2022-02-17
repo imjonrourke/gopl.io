8bc3fd174d0ae50d04d020cbe7d59d07166823f0
Jonathan Rourke <jon.rourke@offerup.com>
2022-02-17 01:58:16 -0500

    feat(ex3.4): build server for serving svg

--- ch3/surface/main.go
+++ ch3/surface/main.go
@@ -12,6 +12,11 @@ import (
 	"math"
 )
 
+import (
+	"net/http"
+	"log"
+)
+
 const (
 	width, height = 600, 320            // canvas size in pixels
 	cells         = 100                 // number of grid cells
@@ -24,20 +29,26 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
-		}
+	handler := func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "image/svg+xml")
+    fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
+      "style='stroke: grey; fill: white; stroke-width: 0.7' "+
+      "width='%d' height='%d'>", width, height)
+    for i := 0; i < cells; i++ {
+      for j := 0; j < cells; j++ {
+        ax, ay := corner(i+1, j)
+        bx, by := corner(i, j)
+        cx, cy := corner(i, j+1)
+        dx, dy := corner(i+1, j+1)
+        fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+          ax, ay, bx, by, cx, cy, dx, dy)
+      }
+    }
+    fmt.Fprintln(w, "</svg>")
 	}
-	fmt.Println("</svg>")
+
+  http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func corner(i, j int) (float64, float64) {
